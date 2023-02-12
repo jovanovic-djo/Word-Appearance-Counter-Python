c702e64db4da57c611109d8a949b14eb37483e0e
Đorđe Jovanović <119633175+jovanovic-djo@users.noreply.github.com>
2023-02-12 23:10:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
 from tkinter import *
-from collections import Counter
-import string
+from tkinter import scrolledtext
 
 def count_words():
-    text = input_field.get("1.0", END)
-    words = [word for word in text.split() if not word[0] in string.punctuation]
-    word_counts = Counter(words)
-    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    input_text = input_field.get("1.0", "end-1c")
+    words = input_text.lower().split()
+    word_count = {}
+
+    for word in words:
+        if word.isalpha():
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+
+    sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+    result = ""
+
+    result = "Total number of different words: " + str(len(word_count)) + "\n\n"
+    for word, count in sorted_words:
+        result += f"{word}: {count}\n"
+
     output_field.delete("1.0", END)
-    output_field.insert(END, "Total number of different words: " + str(len(word_counts)) + "\n\n")
-    for word, count in sorted_word_counts:
-        output_field.insert(END, word + ": " + str(count) + "\n")
+    output_field.insert(END, result)
 
 def sort_alphabetically():
-    text = input_field.get("1.0", END)
-    words = [word for word in text.split() if not word[0] in string.punctuation]
-    word_counts = Counter(words)
-    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[0])
+    input_text = input_field.get("1.0", "end-1c")
+    words = input_text.lower().split()
+    word_count = {}
+
+    for word in words:
+        if word.isalpha():
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+
+    sorted_words = sorted(word_count.items(), key=lambda x: x[0])
+    result = ""
+
+    result = "Total number of different words: " + str(len(word_count)) + "\n\n"
+    for word, count in sorted_words:
+        result += f"{word}: {count}\n"
+
     output_field.delete("1.0", END)
-    output_field.insert(END, "Total number of different words: " + str(len(word_counts)) + "\n\n")
-    for word, count in sorted_word_counts:
-        output_field.insert(END, word + ": " + str(count) + "\n")
+    output_field.insert(END, result)
 
 root = Tk()
-root.geometry("350x400")
+root.geometry("350x450")
+root.title("Word Counter")
 root.config(bg='#1c1c1e')
 
-input_field = Text(root, height=10, width=30, bg='#2b2b2e', fg='#ffffff')
-input_field.pack()
+input_field = scrolledtext.ScrolledText(root, height=10, width=40, bg='#2b2b2e', fg='#ffffff')
+input_field.pack(padx=10, pady=10)
 
-output_field = Text(root, height=10, width=30, bg='#2b2b2e', fg='#ffffff')
-output_field.pack()
+output_field = scrolledtext.ScrolledText(root, height=10, width=40, bg='#2b2b2e', fg='#ffffff')
+output_field.pack(padx=10, pady=5)
 
-count_button = Button(root, text="Count words", command=count_words, bg='#3b3b3e', fg='#ffffff')
-count_button.pack()
+sort_by_count_button = Button(root, text="Sort by count", command=count_words, bg='#3b3b3e', fg='#ffffff')
+sort_by_count_button.pack(pady=5)
 
 sort_alphabetically_button = Button(root, text="Sort alphabetically", command=sort_alphabetically, bg='#3b3b3e', fg='#ffffff')
-sort_alphabetically_button.pack()
+sort_alphabetically_button.pack(pady=5)
 
 root.mainloop()
