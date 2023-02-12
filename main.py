a2b7f9eb48dec5ac67a5874c252b0c417e91ce0f
Đorđe Jovanović <119633175+jovanovic-djo@users.noreply.github.com>
2023-02-12 20:21:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,41 @@
-import tkinter as tk
+from tkinter import *
 from collections import Counter
+import string
 
 def count_words():
-    text = text_input.get("1.0", tk.END)
-    words = text.split()
+    text = input_field.get("1.0", END)
+    words = [word for word in text.split() if not word[0] in string.punctuation]
     word_counts = Counter(words)
-    result = "Word Counts\n"
-    result += "-" * 20 + "\n"
-    for word, count in word_counts.items():
-        if len(word) > 15:
-            result += f"{word[:15]}-\n{word[15:]}: {count}\n"
-        else:
-            result += f"{word}: {count}\n"
-    result += "-" * 20 + "\n"
-    result += f"Total Different Words: {len(word_counts)}"
-    result_text.config(state=tk.NORMAL)
-    result_text.delete("1.0", tk.END)
-    result_text.insert(tk.END, result)
-    result_text.config(state=tk.DISABLED)
+    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    output_field.delete("1.0", END)
+    output_field.insert(END, "Total number of different words: " + str(len(word_counts)) + "\n\n")
+    for word, count in sorted_word_counts:
+        output_field.insert(END, word + ": " + str(count) + "\n")
 
-root = tk.Tk()
-root.title("Word Counter")
-root.config(bg="#1c1c1e")
+def sort_alphabetically():
+    text = input_field.get("1.0", END)
+    words = [word for word in text.split() if not word[0] in string.punctuation]
+    word_counts = Counter(words)
+    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[0])
+    output_field.delete("1.0", END)
+    output_field.insert(END, "Total number of different words: " + str(len(word_counts)) + "\n\n")
+    for word, count in sorted_word_counts:
+        output_field.insert(END, word + ": " + str(count) + "\n")
 
-input_frame = tk.Frame(root, bg="#1c1c1e")
-input_frame.pack(pady=20)
+root = Tk()
+root.geometry("350x400")
+root.config(bg='#1c1c1e')
 
-result_frame = tk.Frame(root, bg="#1c1c1e")
-result_frame.pack(pady=20)
+input_field = Text(root, height=10, width=30, bg='#2b2b2e', fg='#ffffff')
+input_field.pack()
 
-text_label = tk.Label(input_frame, text="Enter Text:", bg="#1c1c1e", fg="white", font=("Helvetica", 14))
-text_label.pack(pady=5)
+output_field = Text(root, height=10, width=30, bg='#2b2b2e', fg='#ffffff')
+output_field.pack()
 
-text_input = tk.Text(input_frame, height=10, width=40, font=("Helvetica", 12), bg="#262629", fg="white")
-text_input.pack(pady=5)
+count_button = Button(root, text="Count words", command=count_words, bg='#3b3b3e', fg='#ffffff')
+count_button.pack()
 
-count_button = tk.Button(input_frame, text="Count Words", command=count_words, bg="#4d79ff", fg="white", font=("Helvetica", 12))
-count_button.pack(pady=20)
-
-sort_label = tk.Label(result_frame, text="Sort By:", bg="#1c1c1e", fg="white", font=("Helvetica", 14))
-sort_label.pack(pady=5)
-
-sort_var = tk.StringVar(value="Alphabetical")
-sort_dropdown = tk.OptionMenu(result_frame, sort_var, "Alphabetical", "Appearance")
-sort_dropdown.config(bg="#262629", fg="white", font=("Helvetica", 12))
-sort_dropdown.pack(pady=5)
-
-scrollbar = tk.Scrollbar(result_frame)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-result_text = tk.Text(result_frame, height=15, width=40, yscrollcommand=scrollbar.set, bg="#262629", fg="white", font=("Helvetica", 12), state=tk.DISABLED)
-result_text.pack()
-
-scrollbar.config(command=result_text.yview)
+sort_alphabetically_button = Button(root, text="Sort alphabetically", command=sort_alphabetically, bg='#3b3b3e', fg='#ffffff')
+sort_alphabetically_button.pack()
 
 root.mainloop()
-
